d3ab6c570e3dbd66b6767340bf08ce9643744f9b
malo <malolc2001@gmail.com>
2022-03-14 16:00:09 +0100

    rzar

--- Main.py
+++ Main.py
@@ -1,10 +1,31 @@
-from operator import index
-from turtle import clear
-from unicodedata import name
 import numpy as np
 import pandas as pd
 
 
-df= pd.read_table('tableauContrainte.txt', index_col=0)
+#df = pd.read_table('tableauContrainte.txt', sep='\t')
 
-print(df.index[0])
+df = pd.read_fwf('tableauContrainte2.txt',
+                 delim_whitespace=True, header=None, engine='python')
+
+print(df)
+
+tabContr = df.to_numpy(dtype=np.int32)
+
+print(tabContr)
+
+
+nbSommets = len(tabContr[:, 1:])
+print(nbSommets, ' sommets')
+
+
+def nbArcs(data):
+    somme = 0
+    taille = len(data) + 2
+
+    for i in range(0, taille):
+        if(data[:i-1, i] < 0):
+            somme += 1
+    return somme
+
+
+print(nbArcs(tabContr), ' arcs')
