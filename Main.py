84e338b9b0c9616f195da0a25572c71104cc7959
malo <malolc2001@gmail.com>
2022-03-15 15:01:59 +0100

    Toutlesarcs

--- Main.py
+++ Main.py
@@ -1,31 +1,48 @@
+from re import A
 import numpy as np
 import pandas as pd
 
 
-#df = pd.read_table('tableauContrainte.txt', sep='\t')
-
 df = pd.read_fwf('tableauContrainte2.txt',
                  delim_whitespace=True, header=None, engine='python')
-
 print(df)
-
 tabContr = df.to_numpy(dtype=np.int32)
 
-print(tabContr)
 
-
-nbSommets = len(tabContr[:, 1:])
-print(nbSommets, ' sommets')
+def nbSommets(data):
+    shape = data.shape
+    return shape[0]
 
 
 def nbArcs(data):
+    shape = data.shape
+    lin = shape[0]
+    col = shape[1]
     somme = 0
-    taille = len(data) + 2
 
-    for i in range(0, taille):
-        if(data[:i-1, i] < 0):
-            somme += 1
+    for i in range(3, col+1):
+        for j in range(0, lin):
+            if(data[:, i-1:i][j] >= 0):
+                somme += 1
     return somme
 
 
-print(nbArcs(tabContr), ' arcs')
+def ToutLesArcs(data):
+    shape = data.shape
+    lin = shape[0]
+    col = shape[1]
+    a = np.array([], dtype=int)
+
+    for i in range(3, col+1):
+        for j in range(0, lin):
+            if(data[:, i-1:i][j] >= 0):
+                a = np.append(a, data[:, i-1:i][j])
+                print(data[:, i-1:i][j])
+    return a
+
+
+print('il y a ', nbSommets(tabContr), ' sommets')
+
+print('il y a ', nbArcs(tabContr), ' arcs')
+
+print(ToutLesArcs(tabContr))
